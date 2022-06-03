7b56093f03be5e64a24723587b2d9e2832045bd9
sercaan1 <sercankarakuyu@gmail.com>
2022-06-03 15:20:59 +0300

    Add project files.

---
+++ TodoListApi/Controllers/TodoItemsController.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using TodoListApi.Data;
+
+namespace TodoListApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TodoItemsController : ControllerBase
+    {
+        private readonly AppDbContext _db;
+
+        public TodoItemsController(AppDbContext db)
+        {
+            _db = db;
+        }
+        [HttpGet]
+        public List<TodoItem> Get()
+        {
+            return _db.TodoItems.OrderBy(x => x.IsDone).ToList();
+        }
+
+
+        [HttpGet("{id}")]
+        public ActionResult<TodoItem> Get(int id)
+        {
+            TodoItem todo = _db.TodoItems.Find(id);
+            if (todo != null)
+                return todo;
+
+            return NotFound();
+        }
+
+        [HttpPost]
+        public ActionResult<TodoItem> Post(TodoItem todoItem)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.TodoItems.Add(todoItem);
+                _db.SaveChanges();
+                return todoItem;
+            }
+
+            return BadRequest(ModelState);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, TodoItem todoItem)
+        {
+            if (id != todoItem.Id)
+                return BadRequest();
+
+            if (ModelState.IsValid)
+            {
+                _db.TodoItems.Update(todoItem);
+                _db.SaveChanges();
+                return NoContent();
+            }
+
+            return BadRequest(ModelState);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            TodoItem todoItem = _db.TodoItems.Find(id);
+
+            if (todoItem != null)
+            {
+                _db.TodoItems.Remove(todoItem);
+                _db.SaveChanges();
+                return NoContent();
+            }
+            return NotFound();
+        }
+    }
+}
---
+++ TodoListApi/Data/AppDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TodoListApi.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<TodoItem> TodoItems { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<TodoItem>().HasData(
+                new TodoItem() { Id = 1, Title = "Do your homework", IsDone = true },
+                new TodoItem() { Id = 2, Title = "Do fitness", IsDone = false },
+                new TodoItem() { Id = 3, Title = "Call your parents", IsDone = false },
+                new TodoItem() { Id = 4, Title = "Clean your room", IsDone = true }
+                );
+        }
+    }
+}
